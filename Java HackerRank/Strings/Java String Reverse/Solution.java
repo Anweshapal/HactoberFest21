3491b8d1e97aaa48cd3a6129e7f5856da1f9a5e7
Anweshapal_ <86425625+Anweshapal@users.noreply.github.com>
2021-10-03 21:14:13 +0530

    please check my changes.

--- Java HackerRank/Strings/Java String Reverse/Solution.java
+++ Java HackerRank/Strings/Java String Reverse/Solution.java
@@ -8,6 +8,7 @@ public class Solution {
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         String reverse = (new StringBuffer(A)).reverse().toString();
+       System.out.println(reverse); // add this line otherwise it'll not show the reverse order
         if (A.equals(reverse)) {
             System.out.println("Yes");
         } else {
